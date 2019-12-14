121b11a1ad23c711508de7f0b3477e51e67f8a36
Daniel A. Thompson <github@danielthompson.net>
2019-12-13 21:31:16 -0600

    additional tests

--- tests/TestFileParser.cpp
+++ tests/TestFileParser.cpp
@@ -10,6 +10,9 @@
 #include "../src/utilities/Logger.h"
 #include "../src/films/PNGFilm.h"
 #include "../src/filters/BoxFilter.h"
+#include "../src/integrators/PathTraceIntegrator.h"
+#include "../src/scenes/NaiveScene.h"
+#include "../src/cameras/PerspectiveCamera.h"
 
 Polytope::Logger Log;
 
@@ -61,6 +64,7 @@ namespace Tests {
          ASSERT_NE(nullptr, runner->Film->Filter);
          ASSERT_NE(nullptr, runner->Integrator);
          ASSERT_NE(nullptr, runner->Integrator->Scene);
+         ASSERT_NE(nullptr, runner->Integrator->Scene->Camera);
          ASSERT_EQ(7, runner->Integrator->MaxDepth);
          ASSERT_NE(nullptr, runner->Scene);
 
@@ -82,9 +86,26 @@ namespace Tests {
          ASSERT_NE(nullptr, actualFilm);
          ASSERT_EQ("minimum.png", actualFilm->Filename);
 
+         // integrator
+         std::unique_ptr<Polytope::AbstractIntegrator> integrator = std::move(runner->Integrator);
+         Polytope::PathTraceIntegrator *actualIntegrator = dynamic_cast<Polytope::PathTraceIntegrator *>(integrator.get());
+         ASSERT_NE(nullptr, actualIntegrator);
 
-         // ensure the scene is a naive scene
-         // TODO std::unique_ptr<Polytope::AbstractScene> scene = std::move(runner->Integrator->Scene);
+         // integrator's scene
+         Polytope::AbstractScene* scene = actualIntegrator->Scene;
+         Polytope::NaiveScene* actualScene = dynamic_cast<Polytope::NaiveScene *>(scene);
+         ASSERT_NE(nullptr, actualScene);
+         ASSERT_EQ(0, actualScene->Lights.size());
+         ASSERT_EQ(0, actualScene->Shapes.size());
+
+         // camera
+         std::unique_ptr<Polytope::AbstractCamera> camera = std::move(scene->Camera);
+         Polytope::PerspectiveCamera* actualCamera = dynamic_cast<Polytope::PerspectiveCamera *>(camera.get());
+         ASSERT_NE(nullptr, actualCamera);
+
+         // TODO fix
+         //  ASSERT_EQ(640, actualCamera->Settings.Bounds.x);
+         //  ASSERT_EQ(640, actualCamera->Settings.Bounds.y);
       }
    }
 }
