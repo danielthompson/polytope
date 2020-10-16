22f214727327f7e59e428de702455722db02995a
Daniel <git@danielthompson.net>
2020-10-16 05:59:01 -0500

    fixed memory bug in png output

--- src/cuda/png_output.cpp
+++ src/cuda/png_output.cpp
@@ -12,21 +12,21 @@ namespace poly {
    void OutputPNG::Output(const poly::GPUMemoryManager* memory_manager) {
       // copy samples from device to host
 
-      const unsigned int bytes = sizeof(float) * memory_manager->num_pixels;
+      const unsigned int num_pixels = memory_manager->num_pixels;
+      const unsigned int num_bytes = num_pixels * sizeof(float);
       
-      float* h_samples_r = (float *)calloc(bytes, 1);
-      float* h_samples_g = (float *)calloc(bytes, 1);
-      float* h_samples_b = (float *)calloc(bytes, 1);
+      float* h_samples_r = (float *)calloc(num_pixels, sizeof(float));
+      float* h_samples_g = (float *)calloc(num_pixels, sizeof(float));
+      float* h_samples_b = (float *)calloc(num_pixels, sizeof(float));
       
-      
-      cuda_check_error( cudaMemcpy(h_samples_r, memory_manager->host_samples.r, bytes, cudaMemcpyDeviceToHost) );
-      cuda_check_error( cudaMemcpy(h_samples_g, memory_manager->host_samples.g, bytes, cudaMemcpyDeviceToHost) );
-      cuda_check_error( cudaMemcpy(h_samples_b, memory_manager->host_samples.b, bytes, cudaMemcpyDeviceToHost) );
+      cuda_check_error( cudaMemcpy(h_samples_r, memory_manager->host_samples.r, num_bytes, cudaMemcpyDeviceToHost) );
+      cuda_check_error( cudaMemcpy(h_samples_g, memory_manager->host_samples.g, num_bytes, cudaMemcpyDeviceToHost) );
+      cuda_check_error( cudaMemcpy(h_samples_b, memory_manager->host_samples.b, num_bytes, cudaMemcpyDeviceToHost) );
       
       // write them to file
-      std::vector<unsigned char> data(4 * bytes);
+      std::vector<unsigned char> data(4 * num_bytes);
 
-      for (unsigned int i = 0; i < bytes; i++) {
+      for (unsigned int i = 0; i < num_pixels; i++) {
          const unsigned int h_index = i;
          
          data[4 * i] = h_samples_r[h_index] > 255 ? 255 : h_samples_r[h_index];
