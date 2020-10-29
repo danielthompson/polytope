dc5754b342896979210629e7b2ab6ec9fd40a89d
Daniel <git@danielthompson.net>
2020-10-29 05:45:38 -0500

    WIP

---
+++ src/cuda/kernels/brdf.cu
@@ -0,0 +1,5 @@
+//
+// Created by daniel on 10/23/20.
+//
+
+#include "brdf.cuh"
---
+++ src/cuda/kernels/brdf.cuh
@@ -0,0 +1,10 @@
+//
+// Created by daniel on 10/23/20.
+//
+
+#ifndef POLYTOPE_BRDF_CUH
+#define POLYTOPE_BRDF_CUH
+
+
+
+#endif //POLYTOPE_BRDF_CUH
